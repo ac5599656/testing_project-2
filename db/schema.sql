0a2eac9799fc39533239c91b39f4fb0c32c83517
Link <username@users.noreply.github.com>
2019-02-13 19:10:43 -0700

    added people table

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE alcoholic_beverages_db;
 CREATE TABLE beers
 (
 	id INT NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+	`name` varchar(100) NOT NULL,
     category VARCHAR(50),
     abv DECIMAL(4,2) NOT NULL,
     volume_OZ DECIMAL(3,1) NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE beers
 CREATE TABLE wines
 (
 	id INT NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+	`name` varchar(100) NOT NULL,
     category VARCHAR(50),
     abv DECIMAL(4,2) NOT NULL,
     volume_OZ DECIMAL(3,1) NOT NULL,
@@ -28,10 +28,22 @@ CREATE TABLE wines
 CREATE TABLE spirits
 (
 	id INT NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+	`name` varchar(100) NOT NULL,
     category VARCHAR(50),
     abv DECIMAL(4,2) NOT NULL,
     volume_OZ DECIMAL(3,1) NOT NULL,
 	consumed BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE people
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	age TINYINT NOT NULL,
+	gender VARCHAR(20) NOT NULL,
+    height VARCHAR(50) NOT NULL,
+    `weight` SMALLINT NOT NULL,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO beers (name, category, abv, volume_OZ)
+INSERT INTO beers (`name`, category, abv, volume_OZ)
 VALUES (
     "Coors Light", "Pilsner", 4.2, 12
 ),
@@ -6,7 +6,7 @@ VALUES (
     "Lagunitas", "IPA", 6.2, 12
 );
 
-INSERT INTO wines (name, category, abv, volume_OZ)
+INSERT INTO wines (`name`, category, abv, volume_OZ)
 VALUES (
     "Kendall-Jackson Grand Reserve", "Cabernet Sauvignon", 13.5, 5
 ),
@@ -14,10 +14,21 @@ VALUES (
     "Giant Steps", "Pinot Noir", 13.5, 5
 );
 
-INSERT INTO spirits (name, category, abv, volume_OZ)
+INSERT INTO spirits (`name`, category, abv, volume_OZ)
 VALUES (
     "Rumple Minze", "schnapps", 50, 1.5
 ),
 (
     "Crown Royal", "whiskey", 40, 1.5
+);
+
+INSERT INTO people (`name`, email, age, gender, height, `weight`)
+VALUES (
+    "Anne", "ac5599656@gmail.com", 40, 'Female', 55, 152
+),
+(
+    "Bonnie", "bonnieacuna@gmail.com", 37, 'Female', 59, 180
+),
+(
+    "Greg", "gregmash12@gmail.com", 29, 'Male', 61, 205
 );
