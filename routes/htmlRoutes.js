5d4ccf2b0c530f9ccaa8bf6c07b6b014d056ff11
Link <username@users.noreply.github.com>
2019-02-20 19:47:05 -0700

    switching to local

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
-// var db = require("../models");
+var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.render("signup");
+    res.render('index', {name:'Some person with a reallyl reallly long name', age:25, location:'Washington D.C.'});
     // db.Example.findAll({}).then(function(dbExamples) {
     //   res.render("index", {
     //     msg: "Welcome!",
@@ -13,13 +13,32 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
+  app.get("/signup.handlebars", function(req, res) {
+      res.render("signup");
+    // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    //   res.render("example", {
+    //     example: dbExample
+    //   });
+    // });
+  });
+
+  app.get("/login.handlebars", function(req, res) {
+    res.render("login");
+  // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //   res.render("example", {
+  //     example: dbExample
   //   });
   // });
+});
+
+app.get("/users", function(req, res) {
+  res.render("users");
+// db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//   res.render("example", {
+//     example: dbExample
+//   });
+// });
+});
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
