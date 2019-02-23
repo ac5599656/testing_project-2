bd9fa35a5354b7aba4edf735ec4a4cebb9e1b1f1
Bonnie <bonnieacuna@gmail.com>
2019-02-23 13:41:26 -0700

    Strange merge

--- models/user.js
+++ models/user.js
@@ -1,53 +1,77 @@
+// var bcrypt = require('bcrypt-nodejs');
+
 module.exports = function(sequelize, DataTypes) {
-    let Users = sequelize.define("Users", {
-      firstname: {
-        type: DataTypes.STRING
-        // allowNull: false,
-        // validate: {
-        //     len: [1]
-        // }
-      },
-      lastname: {
-        type: DataTypes.STRING
-        // allowNull: false,
-        // validate: {
-        //     len: [1]
-        // }
-      },
-      email: {
-        type: DataTypes.STRING
-        // allowNull: false,
-        // validate: {
-        //     len: [1]
-        // }
-      },
-      password: {
-        type: DataTypes.STRING
-        // allowNull: false,
-        // validate: {
-        //     len: [1]
-        // }
-      }
-    });
-    //{
-    //     instanceMethods: {
-    //         generateHash: function (password) {
-    //             return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-    //         },
-    //         validPassword: function (password) {
-    //             return bcrypt.compareSync(password, this.password);
-    //         },
-    //     }
-    // });
-  
-    // Users.hook('beforeCreate', function (user, options) {
-    //     user.password = user.generateHash(user.password);
-    // });
-  
-    return Users;
+  let User = sequelize.define("User", {
+    firstname: {
+      type: DataTypes.STRING
+      // allowNull: false,
+      // validate: {
+      //     len: [1]
+      // }
+    },
+    lastname: {
+      type: DataTypes.STRING
+      // allowNull: false,
+      // validate: {
+      //     len: [1]
+      // }
+    },
+   
+    email: {
+      type: DataTypes.STRING
+      // allowNull: false,
+      // validate: {
+      //     len: [1]
+      // }
+    },
+    age: {
+      type: DataTypes.STRING,
+      // allowNull: false,
+      // validate: {
+      //   len: [1]
+      // }
+    },
+    gender: {
+      type: DataTypes.STRING,
+      // allowNull: false,
+      // validate: {
+      //   len: [1]
+      // }
+    },
+    password: {
+      type: DataTypes.STRING
+      // allowNull: false,
+      // validate: {
+      //     len: [1]
+      // }
+    }
+  });
+  User.associate = (models) => {
+    User.hasMany(models.Post, {
+      onDelete: "cascade"
+    })
   };
-  
-  
-  
-  
-  
+  // User.associate = (models) => {
+  //   User.hasMany(models.Comment, {
+  //     onDelete: "cascade"
+  //   })
+  // };
+
+
+  //{
+  //     instanceMethods: {
+  //         generateHash: function (password) {
+  //             return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  //         },
+  //         validPassword: function (password) {
+  //             return bcrypt.compareSync(password, this.password);
+  //         },
+  //     }
+  // });
+
+  // Users.hook('beforeCreate', function (user, options) {
+  //     user.password = user.generateHash(user.password);
+  // });
+
+  return User;
+};
